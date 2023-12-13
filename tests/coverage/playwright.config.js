087d1c0b6efb35e0d857448862305d88aa68c95c
cenfun <cenfun@gmail.com>
2023-12-13 10:58:18 +0800

    test onEnd

--- tests/coverage/playwright.config.js
+++ tests/coverage/playwright.config.js
@@ -1,16 +1,40 @@
-const name = 'coverage';
+import EC from 'eight-colors';
+
+let coverageResults;
 
 export default {
-    testDir: `../../tests/${name}`,
-    outputDir: `../../docs/${name}`,
+    testDir: '../../tests/coverage',
+    outputDir: '../../docs/coverage',
 
     reporter: [
         ['list'],
         ['monocart-reporter', {
-            name,
-            outputFile: `docs/${name}/index.html`,
+            name: 'My Test Report with Coverage',
+            outputFile: 'docs/coverage/index.html',
+
+            // turn off info logs
+            // logging: 'error',
 
             coverage: {
+                onEnd: (_coverageResults) => {
+                    coverageResults = _coverageResults;
+                }
+            },
+
+            onEnd: (reportData) => {
+                if (coverageResults) {
+                    const { pct, status } = coverageResults.summary;
+                    let value = `${status} (${pct}%)`;
+                    if (status === 'low') {
+                        value = EC.red(value);
+                    } else if (status === 'medium') {
+                        value = EC.yellow(value);
+                    }
+                    reportData.summary.coverage = {
+                        name: 'Coverage',
+                        value
+                    };
+                }
 
             }
         }]
