be83543f3953381aa153d93a6671119a112d806b
cenfun <cenfun@gmail.com>
2024-08-25 19:08:29 +0800

    test case for screenshot comparison

--- tests/shard1/example03.spec.js
+++ tests/shard1/example03.spec.js
@@ -1,22 +1,16 @@
 import { test, expect } from './fixtures.js';
 
-test('@passed or @failed or @flaky', () => {
-    expect(Math.random()).toBeLessThan(0.5);
-});
-
-test('@passed or @flaky', () => {
-    expect(test.info().retry).toBe(Math.random() > 0.5 ? 1 : 0);
-});
-
-test.describe('describe', () => {
-
-    test('screenshot attachment', async ({ page }, testInfo) => {
-        await page.setContent('<h3>Monocart Reporter Screenshot</h3>');
-        const screenshot = await page.screenshot();
-        await testInfo.attach('screenshot', {
-            body: screenshot,
-            contentType: 'image/png'
-        });
+test('screenshot attachment', async ({ page }, testInfo) => {
+    await page.setContent('<h3>Monocart Reporter Screenshot</h3>');
+    const screenshot = await page.screenshot();
+    await testInfo.attach('screenshot', {
+        body: screenshot,
+        contentType: 'image/png'
     });
-
 });
+
+test('screenshot comparison', async ({ page }) => {
+    await page.setContent('<h3>Monocart Screenshot</h3>');
+    await expect(page).toHaveScreenshot();
+});
+
