6cadf2e6bd18ca0342f6939c7f06f9fb2bca5417
cenfun <cenfun@gmail.com>
2024-09-21 12:39:18 +0800

    fix merge for ts

--- mr.config.ts
+++ mr.config.ts
@@ -2,7 +2,7 @@ import type { MonocartReporterOptions } from "monocart-reporter"
 
 const options: MonocartReporterOptions = {
     name: 'My Merged Report',
-    outputFile: 'docs/merged/index.html',
+    outputFile: 'docs/merged-ts/index.html',
 
     onData: async (reportData, dataList) => {
         dataList.forEach((data: any) => {
